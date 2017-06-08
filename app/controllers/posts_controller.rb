2778ad5f05dea19ea6a65e86c05fabd04cafc060
Your Name <you@example.com>
2017-06-08 01:46:31 -0700

    heroku update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+	
+	def index
+		@content_first = 'Sample Text For Blog';
+		@content_second = 'Sample Text For Blog';
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def show
+		
+	end
+
+	def destroy
+		
+	end
+
+end
