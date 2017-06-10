c98332f343e1b6a8f120414993a2fe1eac49a7ba
Your Name <you@example.com>
2017-06-10 15:08:30 -0700

    to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   
+  get 'categories/index'
+
+  get 'categories/edit'
+
+  get 'categories/new'
+
+  get 'categories/show'
+
   root 'home#index'
 
   resources :posts 
+  resources :categories 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
