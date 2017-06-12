2fcd1d82db413e8613be38e33ad16abddaf23374
Your Name <you@example.com>
2017-06-12 03:33:20 -0700

    Blog Complete

--- db/schema.rb
+++ db/schema.rb
@@ -67,9 +67,9 @@ ActiveRecord::Schema.define(version: 20170612085054) do
     t.string   "title"
     t.text     "body"
     t.integer  "category_id"
-    t.integer  "admin_user_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer  "author_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_foreign_key "comments", "posts"
